96b87c9f794cb7821dec7f4c7e8e3a0de95327ce
PatituJunio <134632391+PatituJunio@users.noreply.github.com>
2026-05-13 10:16:16 -0300

    agregar pantalla de inicio
    Agregar niveles

---
+++ Assets/Scripts/AnteojosController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnteojosController : MonoBehaviour
+{
+    
+
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+           
+            GameManager.instance.ActivarAnteojos(); // Llama al controlador
+            Destroy(gameObject);
+
+        }
+    }
+}
